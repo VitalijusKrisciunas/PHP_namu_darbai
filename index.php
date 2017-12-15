605ea6fbeb3f66b5af8b370c24b845b90f3581cc
Vitalijus Krisciunas <34281040+VitalijusKrisciunas@users.noreply.github.com>
2017-12-15 17:13:33 +0200

    Pirmas darbas

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lentele</title>
+        <meta charset="UTF-8">
+        <style>
+            table td {
+                background-color: lightgreen;
+                color: gray;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Lentele</h1>
+        <table border="1">
+            <tr style="background-color:blue; color:white;">
+                <th  colspan="2">Pavadinimas</th>
+                <th>Veiksmas</th>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align:center;">Vaisiai</td>
+            </tr>
+            <tr>
+                <td>Obuolys</td>
+                <td></td>
+                <td><input type="submit" value="Pirkti"></td>
+            </tr>
+            <tr>
+                <td>Apelsinas</td>
+                <td></td>
+                <td><input type="submit" value="Parduoti"></td>
+             </tr>
+             <tr>
+                <td>Kriause</td>
+                <td></td>
+                <td><input type="submit" value="Pirkti"></td>
+            </tr>
+            <tr>
+                <td colspan="3" style="text-align:center;">Darzoves</td>
+            </tr>
+            <tr>
+                <td>Morka</td>
+                <td></td>
+                <td><input type="submit" value="Pirkti"></td>
+            </tr>
+            <tr>
+                <td>Burokas</td>
+                <td></td>
+                <td><input type="submit" value="Parduoti"></td>
+            </tr>
+            <tr>
+                <td>Kopustas</td>
+                <td></td>
+                <td><input type="submit" value="Pirkti"></td>
+            </tr>
+            <tr>
+                <td>Ridikas</td>
+                <td></td>
+                <td><input type="submit" value="Parduoti"></td>
+            </tr>
+            <tr>
+                <td>Zirnis</td>
+                <td></td>
+                <td><input type="submit" value="Pirkti"></td>
+            </tr>
+            <tr>
+                <td>Melionas</td>
+                <td></td>
+                <td><input type="submit" value="Parduoti"></td>
+            </tr>
+        </table>
+    </body>
+</html>
