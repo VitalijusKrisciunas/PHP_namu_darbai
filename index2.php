448891dbcd67030973fb59e45f231b7fc0b59316
Vitalijus Krisciunas <34281040+VitalijusKrisciunas@users.noreply.github.com>
2017-12-19 16:19:24 +0200

    Add files via upload

--- index2.php
+++ index2.php
@@ -10,26 +10,13 @@
     $a = [[1, 3, 4], [2, 5], [2 => 3, 5 => 8], [1, 1, 5 => 1]];
     $dsum = 0;
 
-    foreach($a as $value){
-        $sum = array_sum($value);
-        echo 'Stulpelio suma: '.array_sum($value).'<br>';
-
-        if ($dsum < $sum){
-            $dsum = $sum;
-        }
-    }
-    
-    echo 'Didziausia stulpelio suma: '.$dsum;
-
-    /* $b = array();
-    
-
+    $b = array();
+   
      // Masyvo perindeksacija
     foreach($a as $elem) {
         
           $x = array_values($elem);
           array_push($b, $x);
-          
     }
 
     foreach($b as $elem) {
@@ -37,20 +24,21 @@
         echo '<br>';
 
     }
- 
-    for ($i = 0; $i <= count($b); $i++) {
-            
-        $column = array_column($b, $i);
-        $sum = array_sum($column);
-        if ($sum != 0) {
-        echo 'Daugiamacio masyvo stulpelio suma yra: '.$sum.'<br>';
-        }
 
+    foreach ($b as $key => $value) {
+            
+        $column = array_column($b, $key);
+        $sum = array_sum($column);
+
+        /* if ($sum != 0) {
+        echo 'Daugiamacio masyvo stulpelio suma yra: '.$sum.'<br>';
+        } */
+        echo 'Daugiamacio masyvo stulpelio suma yra: '.$sum.'<br>';
         if ($dsum <= $sum) {
             $dsum = $sum;
         }
     }
-    echo 'Didziausia stulpelio suma yra: '.$dsum; */
+    echo 'Didziausia stulpelio suma yra: '.$dsum;
 
     ?>
     </body>
