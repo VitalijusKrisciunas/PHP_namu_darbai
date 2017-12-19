f40d4032574d8271cc8c51c7244f46e8d9423f2a
Vitalijus Krisciunas <34281040+VitalijusKrisciunas@users.noreply.github.com>
2017-12-19 13:27:41 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,68 +1,22 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Funkcijos</title>
+        <title>Poros</title>
         <meta charset="UTF-8">
     </head>
     <body>
     <?php
-       
-       $a = array(5, 6, 10, 15);
-       $b = array(8,5, 3, 25);
     
-
-        function masvid($x) {
-            $a = null;
-            $b = count($x);
-            foreach ($x as $elem) {
-                $a += $elem;
-            }
-            $a /= $b;
-            return $a;
+    $a = ['Jonas', 'Petras', 'Antanas', 'Povilas'];
+    var_dump($a);
+    foreach($a as $value) {
+        $x = $value;
+        array_shift($a);
+       
+        foreach($a as $value){
+            echo $x.'-'.$value.'<br>';
         }
-
-        echo 'Pirma uzduotis:'."<br>";
-        echo '<br>'.'Atsakymas yra: '.$c = masvid($a) - masvid($b);
-    ?>
-
-    <br>
-    <br>
-    <br>
-
-    <?php
-        echo 'Antra uzduotis:'."<br>"."<br>";
-
-        // Funkcija skaiciaus dalikliams rasti
-
-        function sk_dal($sk){
-
-            $arr = array();
-
-            for ($i = 1; $i < $sk; $i++) {
-                $b = $sk / $i;
-                if ($b == is_int($b)) {
-                    array_push($arr, $i);
-                }
-            }
-            return $arr;
-        }
-
-        // Funkcija ar skaicius yra tobulas
-
-        function sk_tob($sk) {
-
-            if ($sk == array_sum(sk_dal($sk))) {
-                echo 'Skaicius: '.$sk.' yra tobulas'.'<br>';
-            } 
-        }
-
-        // Skaiciu nuo 1 iki 1000 ciklas
-
-        echo 'Tobulo skaiciuas nuo 1 iki 1000 paieska:'.'<br>'.'<br>';
-
-        for ($i = 1; $i <= 1000; $i++) {
-          sk_tob($i);
-        }
+    }
     ?>
     </body>
 </html>
