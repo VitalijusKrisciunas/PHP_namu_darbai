9085ad521559809cee37ac3492ac8374d1bf2aa0
VitalijusKrisciunas <vitalijusk@gmail.com>
2017-12-21 15:13:58 +0200

    Paskutinis taisymas

--- functions.php
+++ functions.php
@@ -1,6 +1,34 @@
 <?php
 
+// Atskiru dalyku vidurkio skaiciavimas
 function vidurkis($arr) {
-    $vidurkis = array_sum($arr) / count($arr);
-    return $vidurkis;
+    $vidurkis = null;
+    foreach ($arr as $elem) {
+        $vidurkis += $elem;
+    }
+    return round($vidurkis / count($arr));
+   
+}
+
+// Bendro vidurkio skaiciavimas
+function bendrasv($arr) {
+    $vidurkis = null;
+    $suma = 0;
+    $dlk = count($arr);
+
+    foreach ($arr as $value) {
+        $sk = count($value);
+        $vidurkis = vidurkis($value);
+        $suma += $vidurkis;   
+    }
+    return round($suma / $dlk);
+}
+
+// Elementu is masyvo isvedimas
+function outelem($arr) {
+    $rezult = '';
+    foreach ($arr as $elem) {
+        $rezult .= ' '.$elem.' ';
+    }
+    return $rezult;
 }
