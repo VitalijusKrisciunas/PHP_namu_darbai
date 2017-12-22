db55d27cd1a3bdeed9bcc7fb0691afd7b99d9960
VitalijusKrisciunas <vitalijusk@gmail.com>
2017-12-22 22:41:45 +0200

    Dadetas vidurkis mazejancia tvarka

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Poros</title>
+        <title>Klases</title>
         <meta charset="UTF-8">
     </head>
     <body>
@@ -13,10 +13,14 @@
 
         $jonotrim = new Trimestras($jj);
         $onostrim = new Trimestras($oo);
+        $petrotrim =new Trimestras($pp);
+        $marestrim =new Trimestras($mm);
 
         $mokiniai = [
             new Mokinys ('Jonas', 'Jonaitis', $jonotrim),
             new Mokinys ('Ona', 'Onaite', $onostrim),
+            new Mokinys ('Petras', 'Petraitis', $petrotrim),
+            new Mokinys ('Mare', 'Maraite', $marestrim),
         ];
 
         echo '<table style="border: 1px solid black">';
@@ -30,18 +34,22 @@
                 <th>'.'M. v.'.'</th>
                 <th style="border: 1px solid black">'.'B. vidurkis'.'</th>
             </tr>';
-            foreach ($mokiniai as $obj) {
-            
-                echo '<tr style="text-align:center;border: 1px solid black">
-                    <td>'.$obj->vardas.' '.$obj->pavarde.'</td>
-                    <td style="border: 1px solid black">'.outelem($obj->trim->trim['lietuviu']).'</td>
-                    <td style="text-align:center">'.vidurkis($obj->trim->trim['lietuviu']).'</td>
-                    <td style="border: 1px solid black">'.outelem($obj->trim->trim['anglu']).'</td>
-                    <td>'.vidurkis($obj->trim->trim['anglu']).'</td>
-                    <td style="border: 1px solid black">'.outelem($obj->trim->trim['matematika']).'</td>
-                    <td>'.vidurkis($obj->trim->trim['matematika']).'</td>
-                    <td style="border: 1px solid black">'.bendrasv($obj->trim->trim).'</td>     
-                </tr>';
+            for ($i = 10; $i > 1; $i--) {
+                foreach ($mokiniai as $obj) {
+                    if (bendrasv($obj->trim->trim) == $i) {
+                
+                        echo '<tr style="text-align:center;border: 1px solid black">
+                            <td>'.$obj->vardas.' '.$obj->pavarde.'</td>
+                            <td style="border: 1px solid black">'.outelem($obj->trim->trim['lietuviu']).'</td>
+                            <td style="text-align:center">'.vidurkis($obj->trim->trim['lietuviu']).'</td>
+                            <td style="border: 1px solid black">'.outelem($obj->trim->trim['anglu']).'</td>
+                            <td>'.vidurkis($obj->trim->trim['anglu']).'</td>
+                            <td style="border: 1px solid black">'.outelem($obj->trim->trim['matematika']).'</td>
+                            <td>'.vidurkis($obj->trim->trim['matematika']).'</td>
+                            <td style="border: 1px solid black">'.bendrasv($obj->trim->trim).'</td>     
+                        </tr>';
+                    }
+                }
             }
             
         echo '</table>';
