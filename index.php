43c0c152d25b177534b578c927ef9c76c84e4124
VitalijusKrisciunas <vitalijusk@gmail.com>
2018-01-09 01:47:26 +0200

    pirmas varijantas

--- index.php
+++ index.php
@@ -1,53 +1,111 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Demo 2</title>
+        <meta charset="UTF-8">
+    </head>
+<body>
+
+<?php 
+    require_once 'db.php';
+    $conn = connectDB();
+    $uri = $_SERVER['PHP_SELF'];
+
+    if (isset($_GET['delete'])) {
+        $sql = "DELETE FROM radars WHERE id = ". intval($_GET['delete']);
+        $conn->query($sql);
+    }
+
+    $row = [
+        'id'=>'',
+        'date'=>'',
+        'number'=>'',
+        'distance'=>'',
+        'time'=>''
+    ];
+
+    if (isset($_GET['edit'])) {
+        $sql = "SELECT * FROM radars WHERE id = ". intval($_GET['edit']);
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();   
+        }
+    }
+
+    if (isset($_POST['save'])) {
+            // formos kintamieji
+            $date = $_POST['date'];
+            $number = $_POST['number'];
+            $distance = $_POST['distance'];
+            $time = $_POST['time'];
+
+        if (intval($_POST['id']) > 0) {
+            echo "update";
+            $sql = "UPDATE radars 
+            SET date = '$date', number = '$number', 
+            distance = $distance, time = $time WHERE id = ". intval($_GET['edit']);
+            $conn->query($sql); 
+            header("Location:$uri");
+        } else {
+            echo "insert";
+            $sql = "INSERT INTO radars(date, number, distance, time) VALUES
+            ('$date', '$number', $distance, $time)";
+            $conn->query($sql);
+        }
+    }
+
+?>
+
+<form method='post'>
+    <input type='hidden' name='id' required value="<?= $row['id'] ?>">
+    Data: <input type='text' name='date' required value="<?= $row['date'] ?>"><br>
+    Numeris: <input type='text' name='number' required value="<?= $row['number'] ?>"><br>
+    Atstumas: <input type='number' name='distance' required value="<?= $row['distance'] ?>"><br>
+    Laikas: <input type='number' name='time' required value="<?= $row['time'] ?>">
+    <button name="save" type="submit">Išsaugoti</button>
+</form>
+
+<hr>
+
 <?php
 
-require_once 'db.php';
-//require_once 'lentele.php';
+    // išvedame automobilius
+    $sql = 'SELECT *, `distance`/`time`*3.6 as `speed` FROM radars ORDER BY number, date DESC';
 
-$conn = connectDB();
+    $result = $conn->query($sql);
 
-// irasu skaicius
-$per_page=10;
-
-// puslapio numerio gavimas
-if (isset($_GET['page'])) $page=($_GET['page']-1); else $page=0;
-
-// pirma LIMIT reiksme
-$start=abs($page*$per_page);
-
-// sudarome uzklausa
-// kintamaji $start naudojame kaip irasu numeratoriu.
-$q="SELECT *, distance/time * 3.6 AS speed FROM `radars` 
-ORDER BY speed DESC LIMIT $start,$per_page";
-$res=$conn->query($q);
-while($row=mysqli_fetch_array($res)) {
-  echo ++$start.". ".$row['number'].' '.round($row['speed'],1)."<br>\n";
-}
-
-// isvedame nuorodas i puslapius:
-$q="SELECT count(*) FROM `radars`";
-$res=$conn->query($q);
-$row=mysqli_fetch_row($res);
-$total_rows=$row[0];
-
-$num_pages=ceil($total_rows/$per_page);
-
-if ($page >= 1) {
-  $i = $_GET['page'] - 1;
-  echo '<a href="'.$_SERVER['PHP_SELF'].'?page='.$i.'">'.'Atgal '.'</a>';
-}
-
-// puslapiu skaiciu isvedimas
-/* for($i=1;$i<=$num_pages;$i++) {
-  if ($i-1 == $page) {
-    echo $i." ";
-  } else {
-    echo '<a href="'.$_SERVER['PHP_SELF'].'?page='.$i.'">'.$i."</a> ";
-  }
-} */
-
-if ($page < $num_pages - 1) {
-  $i = $_GET['page'] + 1;
-  echo '<a href="'.$_SERVER['PHP_SELF'].'?page='.$i.'">'.' Pirmyn'.'</a>';
-}
-
-$conn->close();
+    if ($result->num_rows > 0) {
+        ?>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Numeris</th>
+                <th>Data</th>
+                <th>Atstumas (km)</th>
+                <th>Laikas (h)</th>
+                <th>Greitis (km/h)</th>
+                <th>Veiksmai</th>
+            </tr>
+        
+        <?php while($row = $result->fetch_assoc()): ?>
+            <tr>
+                <td><?= $row['id'] ?></td>
+                <td><?= $row['number'] ?></td>
+                <td><?= $row['date'] ?></td>
+                <td><?= $row['distance'] ?></td>
+                <td><?= $row['time'] ?></td>
+                <td><?= round($row['speed']) ?></td>
+                <td>
+                    <a href="?edit=<?= $row['id'] ?>">Taisyti</a>
+                    <a href="?delete=<?= $row['id'] ?>">Trinti</a>
+                </td>
+            </tr>
+        <?php endwhile; ?>
+        </table>
+        <?php
+    } else {
+        echo 'nėra duomenų';
+    }
+?>
+</body>
+</html>
